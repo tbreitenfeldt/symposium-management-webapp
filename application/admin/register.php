749bc5f131192dbc55bb3682180f6ea963c87805
Chester Southwood <csouthwood@eagles.ewu.edu>
2019-04-09 20:35:50 -0700

    Fixed Footer On Registration and Login

--- application/admin/register.php
+++ application/admin/register.php
@@ -12,52 +12,59 @@ session_write_close();
   <?php require_once "../includes/header.php"; ?>
   <title>Admin Register</title>
 
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="../css/home.css">
   <link rel="stylesheet" href="../css/login.css">
+  <link rel="stylesheet" href="../css/register.css">
 
   <script src="../js/loginSystemJs/loginAJAX.js"></script>
 </head>
 
 <body>
-  <div id="contentId">
 
-      <header>
-        <h1>Register</h1>
-      </header>
+  <header></header>
 
-      <main>
-
-        <form id="userLogonForm" method="POST" action="../loginAPI/registerFunctions.php" type="json" class="form-horizontal">
-          <div id="outputRegion" aria-live="polite" ></div>
-
-            <fieldset>
-
-              <legend>Register Form</legend>
-              <p>Your username must start with a letter, <br/>and may contain only <br/>letters, numbers, dashes, and periods.</p>
-              <div id="fieldClass">
-                  <label for="admin_name" class="control-label">Username:</label>
-                  <input type="text" id="admin_name" name="admin_name" required="required" />
-
-                  <br>
-
-                  <label for="admin_password" class="control-label">Password:</label>
-                  <input type=password id="admin_password" name="admin_password" required="required" />
-
-                  <br>
-
-                  <label for="confirm_password" class="control-label">Confirm Password:</label>
-                  <input type="password" id="confirm_password" name="confirm_password" required="required" />
+  <main class="wrapper">
+    <form id="userLogonForm" method="POST" action="../loginAPI/registerFunctions.php" type="json" class="form-horizontal">
+      <fieldset>
+        <legend>Administrator<br/>Registration Form</legend>
+        <p>Your username must start with a letter, <br/>and may contain only <br/>letters, numbers, dashes, and periods.</p>
+        <div id="fieldClass">
+          <div class="form-group row">
+            <label for="admin_name" class="col-sm-12 col-form-label">Username:</label>
+            <div class="col-sm-12">
+              <input type="text" class="form-control" id="admin_name" name="admin_name" required="required" placeholder="Name">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="admin_password" class="col-sm-12 col-form-label">Password:</label>
+            <div class="col-sm-12">
+              <input type="password" class="form-control" id="admin_password" name="admin_password" required="required" placeholder="********">
+            </div>
+          </div>
+          <div class="form-group row">
+              <label for="confirm_password" class="col-sm-12 col-form-label">Confirm Password:</label>
+              <div class="col-sm-12">
+                <input type="password" class="form-control" id="confirm_password" name="confirm_password" required="required" placeholder="********">
               </div>
-              <input type="submit" id="registerButton" value="Register" />
+            </div>
+          </div>
+          <div class="form-group row justify-content-center">
+            <div class="form-check col-sm-10">
+              <input type="submit" id="registerButton" value="Register" class="btn btn-primary btn-lg btn-block"/>
+            </div>
+          </div>    
+          
+          <div class="form-group row justify-content-center">
+            <div class="form-check">
+                  <p>Already have an account?<br/> <a href="login.php">Login here</a></p>
+            </div>
+          </div>
+      </fieldset>
+      <div id="outputRegion" aria-live="polite" ></div>
+    </form>
 
-            </fieldset>
-            
-            <br>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
-      </form>
-
-      </main>
-  </div>
+  </main>
 
     <?php require_once "../includes/footer.php"; ?>
 
--- application/register.php
+++ application/register.php
@@ -16,83 +16,74 @@ session_write_close();
   <link rel="stylesheet" href="css/home.css">
   <link rel="stylesheet" href="css/login.css">
   <link rel="stylesheet" href="css/register.css">
-
   <script src="js/loginSystemJs/loginAJAX.js"></script>
 </head>
 
 <body>
-<header>
-</header>
-
-<main>
-
-  <form id="userLogonForm" method="POST" action="loginAPI/registerFunctions.php" type="json">
-  <fieldset>
-      <legend>Registration Form</legend>
-      <p>Your username must start with a letter, and may contain only letters, numbers, dashes, and periods.</p>
-
-    <div class="form-group row">
-    <label for="user_name" class="col-sm-3 col-form-label">Username</label>
-    <div class="col-sm-9">
-      <input type="text" class="form-control" placeholder="Username" name="user_name" required="required" />
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="user_password" class="col-sm-3 col-form-label">Password</label>
-    <div class="col-sm-9">
-      <input type="password" class="form-control" id="user_password" name="user_password" required="required" placeholder="Password">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="confirm_password" class="col-sm-3 col-form-label">Confirm Password</label>
-    <div class="col-sm-9">
-      <input type="password" class="form-control"  id="confirm_password" name="confirm_password" required="required" placeholder="Password">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="user_email" class="col-sm-3 col-form-label">Email</label>
-    <div class="col-sm-9">
-      <input type="email" class="form-control" id="user_email" name="user_email" required="required" placeholder="Email">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="user_phone" class="col-sm-3 col-form-label">Phone Number</label>
-    <div class="col-sm-9">
-      <input type="phone" class="form-control" id="user_phone" name="user_phone" placeholder="Phone Number"/>
-    </div>
-  </div>
-  <label for="user_notifyByEmail">Notify me by</label>
-  <div class="form-group row">
-    <div class="form-check">
-      <label for="user_notifyByEmail"><strong>Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong></label>
-      <input class="w3-check" type="checkbox" id="user_notifyByEmail" name="user_notifyByEmail" value="true" checked="checked" />
-    </div>
-  </div>
-  <div class="form-group row">
-    <div class="form-check">
-      <label for="user_notifyByPhone"><strong>Text Message</strong></label>
-      <input class="w3-check" type="checkbox" id="user_notifyByPhone" name="user_notifyByPhone" value="true" />
-    </div>
-  </div>
-  <div class="form-group row justify-content-center">
-    <div class="form-check col-sm-10">
-    <input type="submit" id="registerButton" value="Register" class="btn btn-primary btn-lg btn-block"/>
-    </div>
-  </div>
-  <div class="form-group row justify-content-center">
-    <div class="form-check">
-          <p>Already have an account? <a href="login.php">Login here</a>.</p>
-    </div>
-  </div>
-  </fieldset>
-
-  <div id="outputRegion" aria-live="polite" ></div>
-
-
-  </form>
-</main>
-
+  <header></header>
+  <main>
+    <form id="userLogonForm" method="POST" action="loginAPI/registerFunctions.php" type="json">
+      <fieldset>
+        <legend>Registration Form</legend>
+        <p>Your username must start with a letter, and may contain only letters, numbers, dashes, and periods.</p>
+        <div class="form-group row">
+          <label for="user_name" class="col-sm-12 col-form-label">Username</label>
+          <div class="col-sm-12">
+            <input type="text" class="form-control" placeholder="Username" name="user_name" required="required" />
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="user_password" class="col-sm-12 col-form-label">Password</label>
+          <div class="col-sm-12">
+            <input type="password" class="form-control" id="user_password" name="user_password" required="required" placeholder="Password">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="confirm_password" class="col-sm-12 col-form-label">Confirm Password</label>
+          <div class="col-sm-12">
+            <input type="password" class="form-control"  id="confirm_password" name="confirm_password" required="required" placeholder="Password">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="user_email" class="col-sm-12 col-form-label">Email</label>
+          <div class="col-sm-12">
+            <input type="email" class="form-control" id="user_email" name="user_email" required="required" placeholder="Email">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="user_phone" class="col-sm-12 col-form-label">Phone Number</label>
+          <div class="col-sm-12">
+            <input type="phone" class="form-control" id="user_phone" name="user_phone" placeholder="Phone Number"/>
+          </div>
+        </div>
+        <label for="user_notifyByEmail">Notify me by</label>
+        <div class="form-group row">
+          <div class="form-check">
+            <label class="notification-option" for="user_notifyByEmail"><strong>Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong></label>
+            <input class="w3-check" type="checkbox" id="user_notifyByEmail" name="user_notifyByEmail" value="true" checked="checked" />
+          </div>
+        </div>
+        <div class="form-group row">
+          <div class="form-check">
+            <label class="notification-option" for="user_notifyByPhone"><strong>Text Message</strong></label>
+            <input class="w3-check" type="checkbox" id="user_notifyByPhone" name="user_notifyByPhone" value="true" />
+          </div>
+        </div>
+        <div class="form-group row justify-content-center">
+          <div class="form-check col-sm-10">
+            <input type="submit" id="registerButton" value="Register" class="btn btn-primary btn-lg btn-block"/>
+          </div>
+        </div>
+        <div class="form-group row justify-content-center">
+          <div class="form-check">
+                <p>Already have an account?<br/> <a href="login.php">Login here</a></p>
+          </div>
+        </div>
+      </fieldset>
+      <div id="outputRegion" aria-live="polite" ></div>
+    </form>
+  </main>
 </body>
 <?php require_once "includes/footer.php"; ?>
 </html>

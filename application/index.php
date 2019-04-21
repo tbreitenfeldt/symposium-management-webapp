23f4c120c8f3fd42d1c13179621854a39afd7879
TatyanaHubbard <tatsihubbard@gmail.com>
2019-04-21 16:49:17 -0700

    Add files via upload

--- application/index.php
+++ application/index.php
@@ -1,16 +1,4 @@
-<?php
-session_start();
-$_SESSION["user"] = "user";
-
-require_once "config.php";
-
-if (isset($_SESSION[LOGGEDIN_TOKEN_NAME]) && $_SESSION[LOGGEDIN_TOKEN_NAME]) {
-    header("location: " . LOGGEDIN_LANDING_PAGE_NAME);
-    exit;
-}//end if
-
-session_write_close();
-?>
+<?php require_once "authenticateUser.php"; ?>
 
 <!--NOTE Left and Right Menus are opposite of their variable names-->
 <!DOCTYPE html>
@@ -20,6 +8,8 @@ session_write_close();
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+       
         <script src="js/conferenceAPIJs/databaseFunctions.js"></script>
         <script src="js/userJs/mainSchedule.js"></script>
         <script src="js/userJs/userSchedule.js"></script>
@@ -28,10 +18,8 @@ session_write_close();
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
         <!-- Our Custom CSS -->
         <link rel="stylesheet" href="css/menu.css">
-        <!-- Scrollbar Custom CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
-
         <!-- Font Awesome JS -->
+
         <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
     </head>
@@ -255,7 +243,7 @@ session_write_close();
         <div id="content">
             <div id="innerContent">
                 <div id="conference-table">
-                <header id="h1">Conference Schedule</header>
+                <header id="conferenceNameHeader">Conference Schedule</header>
                     <div id="MainConference">
                         <table id="Conference">
                             <thead>
@@ -282,7 +270,7 @@ session_write_close();
                                     <th>Time End</th>
                                 </tr>
                             </thead>
-                            <tbody>
+                            <tbody id="userConInfo">
                             </tbody>
                         </table>
                     </div>
@@ -291,14 +279,13 @@ session_write_close();
         </div>
         <!-- END content  -->
 
-        <!-- jQuery CDN - Slim version (=without AJAX) -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <!-- jQuery Custom Scroller CDN -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
         <!-- Popper.JS -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
         <!-- Bootstrap JS -->
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-        <!-- jQuery Custom Scroller CDN -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+        
 
         <script src="js/userJs/menu.js"></script>
     </body>

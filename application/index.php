03c4cbec9c8cc4a548c3194837c08075c55ea57c
TatyanaHubbard <tatsihubbard@gmail.com>
2019-04-24 21:13:25 -0700

    Add files via upload

--- application/index.php
+++ application/index.php
@@ -3,40 +3,7 @@
         <!-- content  -->
         <div id="content">
             <div id="innerContent">
-                <div id="conference-table">
-                <header id="conferenceNameHeader">Conference Schedule</header>
-                    <div id="MainConference">
-                        <table id="Conference">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Time Start</th>
-                                    <th>Time End</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-
-                <div id="schedule-table" aria-hidden="false">
-                    <header id="h1" aria-hidden="false">My Schedule</header>
-                    <div id="UserConference" aria-hidden="false">
-                        <table id="UsersCon" aria-hidden="false">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Time Start</th>
-                                    <th>Time End</th>
-                                </tr>
-                            </thead>
-                            <tbody id="userConInfo">
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <header><center> Welcome to our Conference!</center></header>
+                <h1><center> Welcome to our Conference!</center></h1>
             </div>
         </div>
         <!-- END content  -->

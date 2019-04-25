e6577b63af796d88ea24245dae61383165bdf2d0
TatyanaHubbard <tatsihubbard@gmail.com>
2019-04-24 21:20:30 -0700

    Add files via upload

---
+++ application/menuPhp/showSchedule.php
@@ -0,0 +1,22 @@
+<?php ?>
+
+<div id="innerContent">
+<center>
+    <div id="schedule-table" aria-hidden="true">
+        <h1> My Schedule</h1>
+
+        <div id="UserConference" aria-hidden="true">
+                <table id="UsersCon" aria-hidden="true">
+                    <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Time Start</th>
+                                <th>Time End</th>
+                            </tr>
+                    </thead>
+                    <tbody id="userConInfo"></tbody>
+                </table>
+        </div>
+    </div>
+</center>
+</div>

e923b3adc0d5c4bd3a7287005e17ea1269d0ccfe
TJ Breitenfeldt <timothyjb310@gmail.com>
2019-05-12 21:11:33 -0700

    fixed date and time issues for IOS.

--- application/phpIncludes/userHeader.php
+++ application/phpIncludes/userHeader.php
@@ -13,9 +13,9 @@ It is called an HTML shiv.-->
 <![endif]-->
 
 <!--meta tags-->
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
 <!--jQuery CDN-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -23,6 +23,7 @@ It is called an HTML shiv.-->
 <!-- Font Awesome JS -->
 <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
 <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
 
 <!-- custom javascript utilities to be included on all user pages -->
 <script src="js/utilityJs/util.js"></script>

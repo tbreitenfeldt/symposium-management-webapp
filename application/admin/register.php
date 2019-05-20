322925aa48d6bea7ff911c89f11e43b6fcfebda1
Chester Southwood <csouthwood@eagles.ewu.edu>
2019-05-20 14:32:30 -0700

    removed styling on register that was outdated

--- application/admin/register.php
+++ application/admin/register.php
@@ -59,7 +59,7 @@ session_write_close();
               <div class="form-group row justify-content-center">
                 <div class="form-check col-sm-10">
                   <input type="reset" id="resetButton" value="Reset" onclick="return confirm('Are you sure you would like to reset this form?');" />
-                  <input type="submit" id="registerButton" value="Register" class="btn btn-primary btn-lg btn-block"/>
+                  <input type="submit" id="registerButton" value="Register" />
                 </div>
               </div>    
               
